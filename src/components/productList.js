41df5cb6aaacbd548c3062ae45f5cf07e103ae5a
MarinaMoshchenko <82483053+MarinaMoshchenko@users.noreply.github.com>
2021-12-18 15:07:59 -0500

    Initial commit

---
+++ src/components/productList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./productList.css";
+import Product from "./product";
+// Be sure to uncomment this line before trying to use the Product component!
+// import Product from './product'
+
+function ProductList(props) {
+  return (
+    <div className="ProductList">
+      {props.inventory.map((item) => (
+        <Product name={item.name} price={item.price} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
